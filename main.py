26a02c7024ebc06a01dadaa1e3fdb25403ada015
Elden_Robbie <gillcharles97@gmail.com>
2025-08-13 21:47:47 -0400

    Updated main.py / Added circleshape.py and circleshape_backup.py with a class CircleShape().

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -12,6 +12,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        
+        # limit the framerate to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
