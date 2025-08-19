eee51afc4161a9fcc5605eb152b8eb8b957c1c42
Elden_Robbie <gillcharles97@gmail.com>
2025-08-19 03:25:56 -0400

    made the player appear as a triangle at the center of the screen

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -16,6 +18,7 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
