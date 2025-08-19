0226d16fc893f4c45408b91472a4e50a2ed0e8ea
Elden_Robbie <gillcharles97@gmail.com>
2025-08-19 11:53:04 -0400

    added groups (updatable and drawable) and set them both as containers for player

--- main_backup.py
+++ main_backup.py
@@ -4,6 +4,9 @@ from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
@@ -17,9 +20,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
